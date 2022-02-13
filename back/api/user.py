6de52ee7d2563df58690cf6855a08294a7eba57f
serajang99 <serajang1679@gmail.com>
2022-02-13 22:34:40 +0900

    refactor: swagger flask-restx 적용

--- back/api/user.py
+++ back/api/user.py
@@ -1,18 +1,25 @@
 from flask import Blueprint, request, session, jsonify
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
-
+from flask_restx import Api, Resource, reqparse, Namespace
 import re
 
 from models import User
 from db_connect import db
 
 user = Blueprint("user", __name__, url_prefix="/api/user")
+user_api = Namespace("user_api", path="/api/user")
 
 
-@user.route('/temp')
-def testUserApi():
-    if request.method == 'GET':
+# @user.route('/temp')
+# def testUser():
+#     if request.method == 'GET':
+#         gokuma = User.query.filter(User.name == 'gokuma').first()
+#         return str(gokuma.name)
+
+
+@user_api.route('/temp')
+class testUserApi(Resource):
+    def get(self):
         gokuma = User.query.filter(User.name == 'gokuma').first()
         return str(gokuma.name)
-
--- back/app.py
+++ back/app.py
@@ -1,24 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 from models import User
 from db_connect import db
-from api.user import user
+from api.user import user, user_api
+from flask_restx import Api, Resource, reqparse
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_object('config')
-    app.register_blueprint(user)
-    
+    # app.register_blueprint(user)
+
     return app
+
 
 app = create_app()
 db.init_app(app)
 migrate = Migrate(app, db)
 
-@app.route('/')
-def welcome():
-    message = "Gokuma is the best!"
-    return message
+api = Api(app, version='1.0', title='API 문서',
+          description='Swagger 문서', doc="/api-docs")
+
+test_api = api.namespace('test', description='조회 API')
+api.add_namespace(user_api)
+
+
+# @app.route('/')
+# def welcome():
+#     if request.method == 'GET':
+#         message = "Gokuma is the best!"
+#         return message
+
+
+@test_api.route('/')
+class welcomeApi(Resource):
+    # @test_api.response(200, 'Successfully created')
+    # @test_api.response(400, 'Bad Request')
+    def get(self):
+        message = "Gokuma is the best!"
+        return message
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
