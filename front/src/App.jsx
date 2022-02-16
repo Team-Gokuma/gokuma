98dd9be90fb1f3caed54bce80a86cc852cf908ed
minyoung <leemin02_@naver.com>
2022-02-16 20:10:26 +0900

    feat: global-styles 적용

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,24 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/globalStyles';
+import theme from './styles/theme';
+import axios from 'axios';
 
 function App() {
-  const [msg, setMsg] = useState(
-    "이 글자가 보인다면 api서버와 연결이 안된 겁니다."
-  );
+  const [msg, setMsg] = useState('이 글자가 보인다면 api서버와 연결이 안된 겁니다.');
   useEffect(() => {
-    axios("/api/user/temp")
+    axios('/api/user/temp')
       .then((res) => setMsg(res.data))
       .catch(console.log);
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div>
         <p>{msg}</p>
-      </header>
-    </div>
+        <div>home</div>
+      </div>
+    </ThemeProvider>
   );
 }
 
