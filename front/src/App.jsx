fd3e3e86255c9ca09452eef86ec2b0b1d4bf8cb3
minyoung <leemin02_@naver.com>
2022-02-16 20:43:40 +0900

    feat: Route 주소 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/globalStyles';
 import theme from './styles/theme';
@@ -12,13 +13,21 @@ function App() {
       .catch(console.log);
   }, []);
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <p>{msg}</p>
-        <div>home</div>
-      </div>
-    </ThemeProvider>
+        <Routes>
+          <Route path="/" element={<div>home</div>} />
+          <Route path="/recommend" element={<div>레시피 추천받기</div>} />
+          <Route path="/refrige" element={<div>냉장고</div>} />
+          <Route path="/teamIntro" element={<div>팀소개</div>} />
+          <Route path="/login" element={<div>로그인</div>} />
+          <Route path="/signup" element={<div>회원가입</div>} />
+          <Route path="/mypage" element={<div>마이페이지</div>} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
