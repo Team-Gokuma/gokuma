8a72cf9275482647ae76508523bf07b58610490f
serajang99 <serajang1679@gmail.com>
2022-02-18 02:11:40 +0900

    feat: 재료 DB 틀 추가

--- back/models.py
+++ back/models.py
@@ -1,5 +1,7 @@
 from db_connect import db
 
+# User
+
 
 class User(db.Model):
     __tablename__ = "User"
@@ -13,6 +15,8 @@ class User(db.Model):
         self.name = name
         self.email = email
         self.password = password
+
+# ShoppingList
 
 
 class ShoppingList(db.Model):
@@ -30,6 +34,8 @@ class ShoppingList(db.Model):
 
     def __str__(self):
         return "<Todo(user_id={},content={}, checked={})>".format(self.user_id, self.content, self.checked)
+
+# Recipe
 
 
 class Recipe(db.Model):
@@ -102,3 +108,49 @@ class RecipeProcess(db.Model):
 
     def __str__(self):
         return f'{self.cooking_no}\n{self.content}\n'
+
+
+# Ingredients
+
+class IngrdCategory(db.Model):
+    __tablename__ = 'IngrdCategory'
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    category = db.Column(db.Integer, primary_key=True, nullable=False)
+
+    def __init__(self, data):
+        if type(data) is dict:
+            self.name = data['name']
+            self.category = data['category']
+
+
+class Ingredients(db.Model):
+    __tablename__ = 'Ingredients'
+    id = db.Column(db.Integer, nullable=False,
+                   primary_key=True, autoincrement=True)
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    category = db.Column(db.Integer,  db.ForeignKey(
+        'IngrdCategory.category'), nullable=True)
+
+    def __init__(self, data):
+        if type(data) is dict:
+            self.name = data['name']
+            self.category = data['category']
+
+# Refrigerator
+
+
+# class Refrigerator(db.Model):
+#     __tablename__ = 'Refrigerator'
+#     id = db.Column(db.Integer, nullable=False,
+#                    primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+#     content = db.Column(db.VARCHAR(255), nullable=False)
+#     category = db.Column(db.Integer,  db.ForeignKey(
+#         'IngrdCategory.category'), nullable=True)
+#     time = db.Column(db.DateTime, nullable=False)
+
+#     def __init__(self, user_id, content, category, time):
+#         self.user_id = user_id
+#         self.content = content
+#         self.category = category
+#         self.time = time
