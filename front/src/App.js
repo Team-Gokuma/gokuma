db9c12bbd931f28c9a21f32027a179f6711737b5
minyoung <leemin02_@naver.com>
2022-02-18 14:24:23 +0900

    style: prettier 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from './styles/globalStyles';
-import theme from './styles/theme';
-import axios from 'axios';
-import { RecoilRoot} from 'recoil';
-import { Home, Recommend, Login } from './pages';
-import { Header } from './components'
+import { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/globalStyles";
+import theme from "./styles/theme";
+import axios from "axios";
+import { RecoilRoot } from "recoil";
+import { Home, Recommend, Login } from "./pages";
+import { Header } from "./components";
 function App() {
-  const [msg, setMsg] = useState('이 글자가 보인다면 api서버와 연결이 안된 겁니다.');
+  const [msg, setMsg] = useState("이 글자가 보인다면 api서버와 연결이 안된 겁니다.");
   useEffect(() => {
-    axios('/api/user/temp')
+    axios("/api/user/temp")
       .then((res) => setMsg(res.data))
       .catch(console.log);
   }, []);
@@ -20,16 +20,16 @@ function App() {
         <GlobalStyle />
         <p>{msg}</p>
         <RecoilRoot>
-          <Header/>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/recommend" element={<Recommend />} />
-              <Route path="/refrige" element={<div>냉장고</div>} />
-              <Route path="/teamIntro" element={<div>팀소개</div>} />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/signup" element={<div>회원가입</div>} />
-              <Route path="/mypage" element={<div>마이페이지</div>} />
-            </Routes>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recommend" element={<Recommend />} />
+            <Route path="/refrige" element={<div>냉장고</div>} />
+            <Route path="/teamIntro" element={<div>팀소개</div>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<div>회원가입</div>} />
+            <Route path="/mypage" element={<div>마이페이지</div>} />
+          </Routes>
         </RecoilRoot>
       </ThemeProvider>
     </BrowserRouter>
