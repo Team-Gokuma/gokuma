c75f47400efce009bff1d3a5b247905c3fb022e6
minyoung <leemin02_@naver.com>
2022-02-25 02:07:12 +0900

    feat: 텍스트로 추가 임시저장
    - 텍스트로 추가하면 500 에러 발생

--- front/src/api/refrige.jsx
+++ front/src/api/refrige.jsx
@@ -8,3 +8,23 @@ export const ingredientList = async () => {
     console.log(error);
   }
 };
+
+export const addIngredientByImage = async (img) => {
+  try {
+    const response = axios.post("api/refrigerator/recoginition/photo", img);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addIngredientByText = async (ingredient) => {
+  try {
+    const data = ingredient;
+    const response = axios.post("api/refrigerator/recoginition/text", data);
+    console.log(data);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
