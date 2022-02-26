fdc143785fb61a5fc8c19b17b1ed213ecbfff39e
minyoung <leemin02_@naver.com>
2022-02-26 17:44:53 +0900

    feat: 장보기 리스트 냉장고에 있는 재료 API 연결하기

--- front/src/components/shoppinglist/RefrigeIngredient.jsx
+++ front/src/components/shoppinglist/RefrigeIngredient.jsx
@@ -1,31 +1,45 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { ReactComponent as IconRefrige } from "../../asset/icon/refrige.svg";
 import { Button } from "../common/Button";
 
 export const RefrigeIngredient = ({ ingredient }) => {
+  const login = window.sessionStorage.getItem("isLogin");
+
   return (
     <InRefrigeIngredient>
       <div className="titleBox">
         <IconRefrige className="leftIcon" />
         <h3>냉장고에 있는 재료</h3>
       </div>
+
       <div className="refrigeContent">
-        {ingredient.map((item, idx) => {
-          return (
-            <span key={item.name + idx} className="tag">
-              <Button
-                text={item.name}
-                bgcolor={"white"}
-                txtcolor={"lightblack"}
-                height={"32px"}
-                cursor={"default"}
-                round={true}
-                border={"1.9px solid #757575"}
-                padding={"16px 16px"}
-              />
-            </span>
-          );
-        })}
+        {(!login || ingredient.length === 0) && (
+          <Link to="/recommend" className="noIngredient">
+            재료를 저장하고, 냉장고의 재료를 최대한 이용한 레시피를 추천받을 수 있습니다!
+            <br />
+            <br />
+            추천 받으러 가기 ->
+          </Link>
+        )}
+        {login &&
+          ingredient.length > 0 &&
+          ingredient.map((item) => {
+            return (
+              <span key={item.id} className="tag">
+                <Button
+                  text={item.content}
+                  bgcolor={"white"}
+                  txtcolor={"lightblack"}
+                  height={"32px"}
+                  cursor={"default"}
+                  round={true}
+                  border={"1.9px solid #757575"}
+                  padding={"16px 16px"}
+                />
+              </span>
+            );
+          })}
       </div>
     </InRefrigeIngredient>
   );
@@ -40,5 +54,12 @@ const InRefrigeIngredient = styled.div`
   & .tag {
     display: inline-block;
     margin: 12px 4px 0 4px;
+  }
+  & .noIngredient {
+    color: ${({ theme }) => theme.color.black};
+    display: inline-block;
+    margin: 3.5rem;
+    line-height: 1.5;
+    border-bottom-left-radius: 999rem;
   }
 `;
