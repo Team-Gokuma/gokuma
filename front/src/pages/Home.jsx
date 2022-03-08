6da2e37e999bca47039a7e974cb4e878c54c050e
minyoung <leemin02_@naver.com>
2022-03-09 00:40:56 +0900

    feat: 이미지 루트 변경

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,9 +1,50 @@
+import styled from "styled-components";
+
 const Home = () => {
   return (
     <>
-      <div>home</div>
+      <HomeContainer>
+        <MainSection>
+          <div>home</div>
+        </MainSection>
+        <CaptureSection></CaptureSection>
+        <RefrigeSection></RefrigeSection>
+        <ShoppingListSection></ShoppingListSection>
+        <BookmarkSection></BookmarkSection>
+        <TeamIntroSection></TeamIntroSection>
+      </HomeContainer>
     </>
   );
 };
+
+const HomeContainer = styled.section`
+  width: 1200px;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
+const MainSection = styled.div`
+  height: 90vh;
+  border: 1px solid blue;
+`;
+const CaptureSection = styled.div`
+  height: 100vh;
+  border: 1px solid orange;
+`;
+const RefrigeSection = styled.div`
+  height: 100vh;
+  border: 1px solid green;
+`;
+const ShoppingListSection = styled.div`
+  height: 100vh;
+  border: 1px solid purple;
+`;
+const BookmarkSection = styled.div`
+  height: 100vh;
+  border: 1px solid yellow;
+`;
+const TeamIntroSection = styled.div`
+  height: 30vh;
+  border: 1px solid cyan;
+`;
 
 export default Home;
