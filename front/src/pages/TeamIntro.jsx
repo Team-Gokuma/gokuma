db3eb4869e4740781fd9deb4a6b2a7c245dc09d1
jang-hoon <wkdal904@naver.com>
2022-03-12 00:02:31 +0900

    feat: gibhub주소 추가

--- front/src/pages/TeamIntro.jsx
+++ front/src/pages/TeamIntro.jsx
@@ -18,35 +18,35 @@ export default function TeamIntro() {
             name="장서우"
             part="백엔드 / AI"
             intro="제가 해결하지 못하는 문제는 없습니다. 끝까지 파고들어 해결해낸다! 자네는 백엔드의 신이야"
-            git=""
+            git="https://github.com/serajang99"
           />
           <Profile
             image={hoon}
             name="장훈"
             part="백엔드 / 프론트엔드"
             intro="백엔드와 프론트엔드를 오가며 양쪽의 문제상황을 이해시켜주는 자네는 코로나 확진자야"
-            git=""
+            git="https://github.com/wkdal904"
           />
           <Profile
             image={min}
             name="이민영"
             part="프론트엔드"
             intro="걱정인형이지만 그 덕에 모두 완성해 내는 자네는 디자인의 신이야"
-            git=""
+            git="https://github.com/minyopi"
           />
           <Profile
             image={hoo}
             name="전현호"
             part="AI"
             intro="인공지능과 데이터에 대한 지식이 뛰어난 자네가 없었다면 어쩔냉장고는 없었네 자네는 데이터의 신이야"
-            git=""
+            git="https://github.com/cundee"
           />
           <Profile
             image={haa}
             name="백정하"
             part="AI"
             intro="냉철한 시각으로 인공지능의 성능을 최대로 끌어내는 능력을 가진 자제는 인공지능의 신이야"
-            git=""
+            git="https://github.com/junghabaek"
           />
        </Members>
     </MypageContainer>
